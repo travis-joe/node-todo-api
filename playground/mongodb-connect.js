16df2ff3dc2257145c73d13f76f189e2b8576b8d
qzy <weelgod@gmail.com>
2017-04-09 13:12:17 +0800

    Connecting to mongodb server

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,25 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+
+	if(err){
+		console.log('Unale to connect to MongoDB server')
+	}
+	console.log('Connected to MongoDB server')
+	// db.collection('Todos').insertOne({
+	// 	text:'Sth to do',
+	// 	completed: false,
+	// }, (err, res) => {
+	// 	if(err){
+	// 		return console.log('Unable to insert todo', err)
+	// 	}
+	//
+	// 	console.log(JSON.stringify(res.ops, undefined, 2))
+	// })
+	db.collection('User').insertOne({
+		name: 'Travis Joe',
+		age: 30,
+		location: 'HangZhou',
+	})
+	db.close();
+});
