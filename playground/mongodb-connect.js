64a58ee6d5a2092dc2d558f6bc3f444d14268b6a
qzy <weelgod@gmail.com>
2017-04-09 13:51:57 +0800

    6

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,7 @@
-const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// const obj = new ObjectID();
+// console.log(obj)
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 
@@ -16,10 +19,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 	//
 	// 	console.log(JSON.stringify(res.ops, undefined, 2))
 	// })
-	db.collection('User').insertOne({
-		name: 'Travis Joe',
-		age: 30,
-		location: 'HangZhou',
-	})
+
+	// db.collection('User').insertOne({
+	// 	name: 'Travis Joe',
+	// 	age: 30,
+	// 	location: 'HangZhou',
+	// }, (err, res) => {
+	// 	if(err) {
+	// 		return console.log('Unable to insert user', err)
+	// 	}
+	// 	console.log(res.ops[0]._id.getTimestamp());
+	// })
+
 	db.close();
 });
